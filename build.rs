1520c877f55a6c885e75ee59024c19597b1030a2
Martin Moreira de Jesus <martin.moreira-de-jesus@clicnwork.io>
2023-06-20 20:36:52 +0200

    refactor: fix type in delete repository rpc and rename service (#2)
    * hotfix: return the right type in delete repository
    
    * refactor: rename gitsprout to git-repo-manager  and move proto file
    
    * hotfix(ci): lint protobuf files in proto dir

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("src/gitsprout.proto")?;
+    tonic_build::compile_protos("proto/git-repo-manager.proto")?;
 
     Ok(())
 }
