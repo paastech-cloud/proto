9725416dc99a1b8af18d2c30a5528865f32362af
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-07-02 13:03:24 +0200

    fix(types): add nestjs flag to type generation for TS

--- types/proto/git-repo-manager.ts
+++ types/proto/git-repo-manager.ts
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import * as _m0 from "protobufjs/minimal";
+import { GrpcMethod, GrpcStreamMethod } from "@nestjs/microservices";
+import { Observable } from "rxjs";
 
 export const protobufPackage = "gitrepomanager";
 
@@ -11,161 +12,37 @@ export interface RepositoryResponse {
   message: string;
 }
 
-function createBaseRepositoryRequest(): RepositoryRequest {
-  return { repositoryPath: "" };
+export const GITREPOMANAGER_PACKAGE_NAME = "gitrepomanager";
+
+export interface GitRepoManagerClient {
+  createRepository(request: RepositoryRequest): Observable<RepositoryResponse>;
+
+  deleteRepository(request: RepositoryRequest): Observable<RepositoryResponse>;
 }
 
-export const RepositoryRequest = {
-  encode(message: RepositoryRequest, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.repositoryPath !== "") {
-      writer.uint32(10).string(message.repositoryPath);
+export interface GitRepoManagerController {
+  createRepository(
+    request: RepositoryRequest,
+  ): Promise<RepositoryResponse> | Observable<RepositoryResponse> | RepositoryResponse;
+
+  deleteRepository(
+    request: RepositoryRequest,
+  ): Promise<RepositoryResponse> | Observable<RepositoryResponse> | RepositoryResponse;
+}
+
+export function GitRepoManagerControllerMethods() {
+  return function (constructor: Function) {
+    const grpcMethods: string[] = ["createRepository", "deleteRepository"];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod("GitRepoManager", method)(constructor.prototype[method], method, descriptor);
     }
-    return writer;
-  },
-
-  decode(input: _m0.Reader | Uint8Array, length?: number): RepositoryRequest {
-    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseRepositoryRequest();
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-      switch (tag >>> 3) {
-        case 1:
-          if (tag !== 10) {
-            break;
-          }
-
-          message.repositoryPath = reader.string();
-          continue;
-      }
-      if ((tag & 7) === 4 || tag === 0) {
-        break;
-      }
-      reader.skipType(tag & 7);
+    const grpcStreamMethods: string[] = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod("GitRepoManager", method)(constructor.prototype[method], method, descriptor);
     }
-    return message;
-  },
-
-  fromJSON(object: any): RepositoryRequest {
-    return { repositoryPath: isSet(object.repositoryPath) ? String(object.repositoryPath) : "" };
-  },
-
-  toJSON(message: RepositoryRequest): unknown {
-    const obj: any = {};
-    message.repositoryPath !== undefined && (obj.repositoryPath = message.repositoryPath);
-    return obj;
-  },
-
-  create<I extends Exact<DeepPartial<RepositoryRequest>, I>>(base?: I): RepositoryRequest {
-    return RepositoryRequest.fromPartial(base ?? {});
-  },
-
-  fromPartial<I extends Exact<DeepPartial<RepositoryRequest>, I>>(object: I): RepositoryRequest {
-    const message = createBaseRepositoryRequest();
-    message.repositoryPath = object.repositoryPath ?? "";
-    return message;
-  },
-};
-
-function createBaseRepositoryResponse(): RepositoryResponse {
-  return { message: "" };
+  };
 }
 
-export const RepositoryResponse = {
-  encode(message: RepositoryResponse, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.message !== "") {
-      writer.uint32(10).string(message.message);
-    }
-    return writer;
-  },
-
-  decode(input: _m0.Reader | Uint8Array, length?: number): RepositoryResponse {
-    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseRepositoryResponse();
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-      switch (tag >>> 3) {
-        case 1:
-          if (tag !== 10) {
-            break;
-          }
-
-          message.message = reader.string();
-          continue;
-      }
-      if ((tag & 7) === 4 || tag === 0) {
-        break;
-      }
-      reader.skipType(tag & 7);
-    }
-    return message;
-  },
-
-  fromJSON(object: any): RepositoryResponse {
-    return { message: isSet(object.message) ? String(object.message) : "" };
-  },
-
-  toJSON(message: RepositoryResponse): unknown {
-    const obj: any = {};
-    message.message !== undefined && (obj.message = message.message);
-    return obj;
-  },
-
-  create<I extends Exact<DeepPartial<RepositoryResponse>, I>>(base?: I): RepositoryResponse {
-    return RepositoryResponse.fromPartial(base ?? {});
-  },
-
-  fromPartial<I extends Exact<DeepPartial<RepositoryResponse>, I>>(object: I): RepositoryResponse {
-    const message = createBaseRepositoryResponse();
-    message.message = object.message ?? "";
-    return message;
-  },
-};
-
-export interface GitRepoManager {
-  CreateRepository(request: RepositoryRequest): Promise<RepositoryResponse>;
-  DeleteRepository(request: RepositoryRequest): Promise<RepositoryResponse>;
-}
-
-export const GitRepoManagerServiceName = "gitrepomanager.GitRepoManager";
-export class GitRepoManagerClientImpl implements GitRepoManager {
-  private readonly rpc: Rpc;
-  private readonly service: string;
-  constructor(rpc: Rpc, opts?: { service?: string }) {
-    this.service = opts?.service || GitRepoManagerServiceName;
-    this.rpc = rpc;
-    this.CreateRepository = this.CreateRepository.bind(this);
-    this.DeleteRepository = this.DeleteRepository.bind(this);
-  }
-  CreateRepository(request: RepositoryRequest): Promise<RepositoryResponse> {
-    const data = RepositoryRequest.encode(request).finish();
-    const promise = this.rpc.request(this.service, "CreateRepository", data);
-    return promise.then((data) => RepositoryResponse.decode(_m0.Reader.create(data)));
-  }
-
-  DeleteRepository(request: RepositoryRequest): Promise<RepositoryResponse> {
-    const data = RepositoryRequest.encode(request).finish();
-    const promise = this.rpc.request(this.service, "DeleteRepository", data);
-    return promise.then((data) => RepositoryResponse.decode(_m0.Reader.create(data)));
-  }
-}
-
-interface Rpc {
-  request(service: string, method: string, data: Uint8Array): Promise<Uint8Array>;
-}
-
-type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
-
-export type DeepPartial<T> = T extends Builtin ? T
-  : T extends Array<infer U> ? Array<DeepPartial<U>> : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>>
-  : T extends {} ? { [K in keyof T]?: DeepPartial<T[K]> }
-  : Partial<T>;
-
-type KeysOfUnion<T> = T extends T ? keyof T : never;
-export type Exact<P, I extends P> = P extends Builtin ? P
-  : P & { [K in keyof P]: Exact<P[K], I[K]> } & { [K in Exclude<keyof I, KeysOfUnion<P>>]: never };
-
-function isSet(value: any): boolean {
-  return value !== null && value !== undefined;
-}
+export const GIT_REPO_MANAGER_SERVICE_NAME = "GitRepoManager";
--- types/proto/pomegranate.ts
+++ types/proto/pomegranate.ts
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import * as _m0 from "protobufjs/minimal";
+import { GrpcMethod, GrpcStreamMethod } from "@nestjs/microservices";
+import { Observable } from "rxjs";
 
 export const protobufPackage = "pomegranate";
 
@@ -31,473 +32,68 @@ export interface ResponseMessage {
   message: string;
 }
 
-function createBaseStartDeploymentRequest(): StartDeploymentRequest {
-  return { deploymentUuid: "" };
+export const POMEGRANATE_PACKAGE_NAME = "pomegranate";
+
+export interface PomegranateClient {
+  startDeployment(request: StartDeploymentRequest): Observable<ResponseMessage>;
+
+  restartDeployment(request: RestartDeploymentRequest): Observable<ResponseMessage>;
+
+  deleteDeployment(request: DeleteDeploymentRequest): Observable<ResponseMessage>;
+
+  stopDeployment(request: StopDeploymentRequest): Observable<ResponseMessage>;
+
+  deploymentStatus(request: DeploymentStatusRequest): Observable<ResponseMessage>;
+
+  applyConfigDeployment(request: ApplyConfigDeploymentRequest): Observable<ResponseMessage>;
 }
 
-export const StartDeploymentRequest = {
-  encode(message: StartDeploymentRequest, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.deploymentUuid !== "") {
-      writer.uint32(10).string(message.deploymentUuid);
-    }
-    return writer;
-  },
+export interface PomegranateController {
+  startDeployment(
+    request: StartDeploymentRequest,
+  ): Promise<ResponseMessage> | Observable<ResponseMessage> | ResponseMessage;
 
-  decode(input: _m0.Reader | Uint8Array, length?: number): StartDeploymentRequest {
-    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseStartDeploymentRequest();
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-      switch (tag >>> 3) {
-        case 1:
-          if (tag !== 10) {
-            break;
-          }
+  restartDeployment(
+    request: RestartDeploymentRequest,
+  ): Promise<ResponseMessage> | Observable<ResponseMessage> | ResponseMessage;
 
-          message.deploymentUuid = reader.string();
-          continue;
-      }
-      if ((tag & 7) === 4 || tag === 0) {
-        break;
-      }
-      reader.skipType(tag & 7);
-    }
-    return message;
-  },
+  deleteDeployment(
+    request: DeleteDeploymentRequest,
+  ): Promise<ResponseMessage> | Observable<ResponseMessage> | ResponseMessage;
 
-  fromJSON(object: any): StartDeploymentRequest {
-    return { deploymentUuid: isSet(object.deploymentUuid) ? String(object.deploymentUuid) : "" };
-  },
+  stopDeployment(
+    request: StopDeploymentRequest,
+  ): Promise<ResponseMessage> | Observable<ResponseMessage> | ResponseMessage;
 
-  toJSON(message: StartDeploymentRequest): unknown {
-    const obj: any = {};
-    message.deploymentUuid !== undefined && (obj.deploymentUuid = message.deploymentUuid);
-    return obj;
-  },
+  deploymentStatus(
+    request: DeploymentStatusRequest,
+  ): Promise<ResponseMessage> | Observable<ResponseMessage> | ResponseMessage;
 
-  create<I extends Exact<DeepPartial<StartDeploymentRequest>, I>>(base?: I): StartDeploymentRequest {
-    return StartDeploymentRequest.fromPartial(base ?? {});
-  },
-
-  fromPartial<I extends Exact<DeepPartial<StartDeploymentRequest>, I>>(object: I): StartDeploymentRequest {
-    const message = createBaseStartDeploymentRequest();
-    message.deploymentUuid = object.deploymentUuid ?? "";
-    return message;
-  },
-};
-
-function createBaseRestartDeploymentRequest(): RestartDeploymentRequest {
-  return { deploymentUuid: "" };
+  applyConfigDeployment(
+    request: ApplyConfigDeploymentRequest,
+  ): Promise<ResponseMessage> | Observable<ResponseMessage> | ResponseMessage;
 }
 
-export const RestartDeploymentRequest = {
-  encode(message: RestartDeploymentRequest, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.deploymentUuid !== "") {
-      writer.uint32(10).string(message.deploymentUuid);
+export function PomegranateControllerMethods() {
+  return function (constructor: Function) {
+    const grpcMethods: string[] = [
+      "startDeployment",
+      "restartDeployment",
+      "deleteDeployment",
+      "stopDeployment",
+      "deploymentStatus",
+      "applyConfigDeployment",
+    ];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod("Pomegranate", method)(constructor.prototype[method], method, descriptor);
     }
-    return writer;
-  },
-
-  decode(input: _m0.Reader | Uint8Array, length?: number): RestartDeploymentRequest {
-    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseRestartDeploymentRequest();
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-      switch (tag >>> 3) {
-        case 1:
-          if (tag !== 10) {
-            break;
-          }
-
-          message.deploymentUuid = reader.string();
-          continue;
-      }
-      if ((tag & 7) === 4 || tag === 0) {
-        break;
-      }
-      reader.skipType(tag & 7);
+    const grpcStreamMethods: string[] = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod("Pomegranate", method)(constructor.prototype[method], method, descriptor);
     }
-    return message;
-  },
-
-  fromJSON(object: any): RestartDeploymentRequest {
-    return { deploymentUuid: isSet(object.deploymentUuid) ? String(object.deploymentUuid) : "" };
-  },
-
-  toJSON(message: RestartDeploymentRequest): unknown {
-    const obj: any = {};
-    message.deploymentUuid !== undefined && (obj.deploymentUuid = message.deploymentUuid);
-    return obj;
-  },
-
-  create<I extends Exact<DeepPartial<RestartDeploymentRequest>, I>>(base?: I): RestartDeploymentRequest {
-    return RestartDeploymentRequest.fromPartial(base ?? {});
-  },
-
-  fromPartial<I extends Exact<DeepPartial<RestartDeploymentRequest>, I>>(object: I): RestartDeploymentRequest {
-    const message = createBaseRestartDeploymentRequest();
-    message.deploymentUuid = object.deploymentUuid ?? "";
-    return message;
-  },
-};
-
-function createBaseDeleteDeploymentRequest(): DeleteDeploymentRequest {
-  return { deploymentUuid: "" };
+  };
 }
 
-export const DeleteDeploymentRequest = {
-  encode(message: DeleteDeploymentRequest, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.deploymentUuid !== "") {
-      writer.uint32(10).string(message.deploymentUuid);
-    }
-    return writer;
-  },
-
-  decode(input: _m0.Reader | Uint8Array, length?: number): DeleteDeploymentRequest {
-    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseDeleteDeploymentRequest();
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-      switch (tag >>> 3) {
-        case 1:
-          if (tag !== 10) {
-            break;
-          }
-
-          message.deploymentUuid = reader.string();
-          continue;
-      }
-      if ((tag & 7) === 4 || tag === 0) {
-        break;
-      }
-      reader.skipType(tag & 7);
-    }
-    return message;
-  },
-
-  fromJSON(object: any): DeleteDeploymentRequest {
-    return { deploymentUuid: isSet(object.deploymentUuid) ? String(object.deploymentUuid) : "" };
-  },
-
-  toJSON(message: DeleteDeploymentRequest): unknown {
-    const obj: any = {};
-    message.deploymentUuid !== undefined && (obj.deploymentUuid = message.deploymentUuid);
-    return obj;
-  },
-
-  create<I extends Exact<DeepPartial<DeleteDeploymentRequest>, I>>(base?: I): DeleteDeploymentRequest {
-    return DeleteDeploymentRequest.fromPartial(base ?? {});
-  },
-
-  fromPartial<I extends Exact<DeepPartial<DeleteDeploymentRequest>, I>>(object: I): DeleteDeploymentRequest {
-    const message = createBaseDeleteDeploymentRequest();
-    message.deploymentUuid = object.deploymentUuid ?? "";
-    return message;
-  },
-};
-
-function createBaseStopDeploymentRequest(): StopDeploymentRequest {
-  return { deploymentUuid: "" };
-}
-
-export const StopDeploymentRequest = {
-  encode(message: StopDeploymentRequest, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.deploymentUuid !== "") {
-      writer.uint32(10).string(message.deploymentUuid);
-    }
-    return writer;
-  },
-
-  decode(input: _m0.Reader | Uint8Array, length?: number): StopDeploymentRequest {
-    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseStopDeploymentRequest();
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-      switch (tag >>> 3) {
-        case 1:
-          if (tag !== 10) {
-            break;
-          }
-
-          message.deploymentUuid = reader.string();
-          continue;
-      }
-      if ((tag & 7) === 4 || tag === 0) {
-        break;
-      }
-      reader.skipType(tag & 7);
-    }
-    return message;
-  },
-
-  fromJSON(object: any): StopDeploymentRequest {
-    return { deploymentUuid: isSet(object.deploymentUuid) ? String(object.deploymentUuid) : "" };
-  },
-
-  toJSON(message: StopDeploymentRequest): unknown {
-    const obj: any = {};
-    message.deploymentUuid !== undefined && (obj.deploymentUuid = message.deploymentUuid);
-    return obj;
-  },
-
-  create<I extends Exact<DeepPartial<StopDeploymentRequest>, I>>(base?: I): StopDeploymentRequest {
-    return StopDeploymentRequest.fromPartial(base ?? {});
-  },
-
-  fromPartial<I extends Exact<DeepPartial<StopDeploymentRequest>, I>>(object: I): StopDeploymentRequest {
-    const message = createBaseStopDeploymentRequest();
-    message.deploymentUuid = object.deploymentUuid ?? "";
-    return message;
-  },
-};
-
-function createBaseDeploymentStatusRequest(): DeploymentStatusRequest {
-  return { deploymentUuid: "" };
-}
-
-export const DeploymentStatusRequest = {
-  encode(message: DeploymentStatusRequest, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.deploymentUuid !== "") {
-      writer.uint32(10).string(message.deploymentUuid);
-    }
-    return writer;
-  },
-
-  decode(input: _m0.Reader | Uint8Array, length?: number): DeploymentStatusRequest {
-    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseDeploymentStatusRequest();
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-      switch (tag >>> 3) {
-        case 1:
-          if (tag !== 10) {
-            break;
-          }
-
-          message.deploymentUuid = reader.string();
-          continue;
-      }
-      if ((tag & 7) === 4 || tag === 0) {
-        break;
-      }
-      reader.skipType(tag & 7);
-    }
-    return message;
-  },
-
-  fromJSON(object: any): DeploymentStatusRequest {
-    return { deploymentUuid: isSet(object.deploymentUuid) ? String(object.deploymentUuid) : "" };
-  },
-
-  toJSON(message: DeploymentStatusRequest): unknown {
-    const obj: any = {};
-    message.deploymentUuid !== undefined && (obj.deploymentUuid = message.deploymentUuid);
-    return obj;
-  },
-
-  create<I extends Exact<DeepPartial<DeploymentStatusRequest>, I>>(base?: I): DeploymentStatusRequest {
-    return DeploymentStatusRequest.fromPartial(base ?? {});
-  },
-
-  fromPartial<I extends Exact<DeepPartial<DeploymentStatusRequest>, I>>(object: I): DeploymentStatusRequest {
-    const message = createBaseDeploymentStatusRequest();
-    message.deploymentUuid = object.deploymentUuid ?? "";
-    return message;
-  },
-};
-
-function createBaseApplyConfigDeploymentRequest(): ApplyConfigDeploymentRequest {
-  return { config: "" };
-}
-
-export const ApplyConfigDeploymentRequest = {
-  encode(message: ApplyConfigDeploymentRequest, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.config !== "") {
-      writer.uint32(10).string(message.config);
-    }
-    return writer;
-  },
-
-  decode(input: _m0.Reader | Uint8Array, length?: number): ApplyConfigDeploymentRequest {
-    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseApplyConfigDeploymentRequest();
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-      switch (tag >>> 3) {
-        case 1:
-          if (tag !== 10) {
-            break;
-          }
-
-          message.config = reader.string();
-          continue;
-      }
-      if ((tag & 7) === 4 || tag === 0) {
-        break;
-      }
-      reader.skipType(tag & 7);
-    }
-    return message;
-  },
-
-  fromJSON(object: any): ApplyConfigDeploymentRequest {
-    return { config: isSet(object.config) ? String(object.config) : "" };
-  },
-
-  toJSON(message: ApplyConfigDeploymentRequest): unknown {
-    const obj: any = {};
-    message.config !== undefined && (obj.config = message.config);
-    return obj;
-  },
-
-  create<I extends Exact<DeepPartial<ApplyConfigDeploymentRequest>, I>>(base?: I): ApplyConfigDeploymentRequest {
-    return ApplyConfigDeploymentRequest.fromPartial(base ?? {});
-  },
-
-  fromPartial<I extends Exact<DeepPartial<ApplyConfigDeploymentRequest>, I>>(object: I): ApplyConfigDeploymentRequest {
-    const message = createBaseApplyConfigDeploymentRequest();
-    message.config = object.config ?? "";
-    return message;
-  },
-};
-
-function createBaseResponseMessage(): ResponseMessage {
-  return { message: "" };
-}
-
-export const ResponseMessage = {
-  encode(message: ResponseMessage, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.message !== "") {
-      writer.uint32(10).string(message.message);
-    }
-    return writer;
-  },
-
-  decode(input: _m0.Reader | Uint8Array, length?: number): ResponseMessage {
-    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseResponseMessage();
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-      switch (tag >>> 3) {
-        case 1:
-          if (tag !== 10) {
-            break;
-          }
-
-          message.message = reader.string();
-          continue;
-      }
-      if ((tag & 7) === 4 || tag === 0) {
-        break;
-      }
-      reader.skipType(tag & 7);
-    }
-    return message;
-  },
-
-  fromJSON(object: any): ResponseMessage {
-    return { message: isSet(object.message) ? String(object.message) : "" };
-  },
-
-  toJSON(message: ResponseMessage): unknown {
-    const obj: any = {};
-    message.message !== undefined && (obj.message = message.message);
-    return obj;
-  },
-
-  create<I extends Exact<DeepPartial<ResponseMessage>, I>>(base?: I): ResponseMessage {
-    return ResponseMessage.fromPartial(base ?? {});
-  },
-
-  fromPartial<I extends Exact<DeepPartial<ResponseMessage>, I>>(object: I): ResponseMessage {
-    const message = createBaseResponseMessage();
-    message.message = object.message ?? "";
-    return message;
-  },
-};
-
-export interface Pomegranate {
-  StartDeployment(request: StartDeploymentRequest): Promise<ResponseMessage>;
-  RestartDeployment(request: RestartDeploymentRequest): Promise<ResponseMessage>;
-  DeleteDeployment(request: DeleteDeploymentRequest): Promise<ResponseMessage>;
-  StopDeployment(request: StopDeploymentRequest): Promise<ResponseMessage>;
-  DeploymentStatus(request: DeploymentStatusRequest): Promise<ResponseMessage>;
-  ApplyConfigDeployment(request: ApplyConfigDeploymentRequest): Promise<ResponseMessage>;
-}
-
-export const PomegranateServiceName = "pomegranate.Pomegranate";
-export class PomegranateClientImpl implements Pomegranate {
-  private readonly rpc: Rpc;
-  private readonly service: string;
-  constructor(rpc: Rpc, opts?: { service?: string }) {
-    this.service = opts?.service || PomegranateServiceName;
-    this.rpc = rpc;
-    this.StartDeployment = this.StartDeployment.bind(this);
-    this.RestartDeployment = this.RestartDeployment.bind(this);
-    this.DeleteDeployment = this.DeleteDeployment.bind(this);
-    this.StopDeployment = this.StopDeployment.bind(this);
-    this.DeploymentStatus = this.DeploymentStatus.bind(this);
-    this.ApplyConfigDeployment = this.ApplyConfigDeployment.bind(this);
-  }
-  StartDeployment(request: StartDeploymentRequest): Promise<ResponseMessage> {
-    const data = StartDeploymentRequest.encode(request).finish();
-    const promise = this.rpc.request(this.service, "StartDeployment", data);
-    return promise.then((data) => ResponseMessage.decode(_m0.Reader.create(data)));
-  }
-
-  RestartDeployment(request: RestartDeploymentRequest): Promise<ResponseMessage> {
-    const data = RestartDeploymentRequest.encode(request).finish();
-    const promise = this.rpc.request(this.service, "RestartDeployment", data);
-    return promise.then((data) => ResponseMessage.decode(_m0.Reader.create(data)));
-  }
-
-  DeleteDeployment(request: DeleteDeploymentRequest): Promise<ResponseMessage> {
-    const data = DeleteDeploymentRequest.encode(request).finish();
-    const promise = this.rpc.request(this.service, "DeleteDeployment", data);
-    return promise.then((data) => ResponseMessage.decode(_m0.Reader.create(data)));
-  }
-
-  StopDeployment(request: StopDeploymentRequest): Promise<ResponseMessage> {
-    const data = StopDeploymentRequest.encode(request).finish();
-    const promise = this.rpc.request(this.service, "StopDeployment", data);
-    return promise.then((data) => ResponseMessage.decode(_m0.Reader.create(data)));
-  }
-
-  DeploymentStatus(request: DeploymentStatusRequest): Promise<ResponseMessage> {
-    const data = DeploymentStatusRequest.encode(request).finish();
-    const promise = this.rpc.request(this.service, "DeploymentStatus", data);
-    return promise.then((data) => ResponseMessage.decode(_m0.Reader.create(data)));
-  }
-
-  ApplyConfigDeployment(request: ApplyConfigDeploymentRequest): Promise<ResponseMessage> {
-    const data = ApplyConfigDeploymentRequest.encode(request).finish();
-    const promise = this.rpc.request(this.service, "ApplyConfigDeployment", data);
-    return promise.then((data) => ResponseMessage.decode(_m0.Reader.create(data)));
-  }
-}
-
-interface Rpc {
-  request(service: string, method: string, data: Uint8Array): Promise<Uint8Array>;
-}
-
-type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
-
-export type DeepPartial<T> = T extends Builtin ? T
-  : T extends Array<infer U> ? Array<DeepPartial<U>> : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>>
-  : T extends {} ? { [K in keyof T]?: DeepPartial<T[K]> }
-  : Partial<T>;
-
-type KeysOfUnion<T> = T extends T ? keyof T : never;
-export type Exact<P, I extends P> = P extends Builtin ? P
-  : P & { [K in keyof P]: Exact<P[K], I[K]> } & { [K in Exclude<keyof I, KeysOfUnion<P>>]: never };
-
-function isSet(value: any): boolean {
-  return value !== null && value !== undefined;
-}
+export const POMEGRANATE_SERVICE_NAME = "Pomegranate";
